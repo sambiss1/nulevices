204c852a5f18e3b2254250edcf88a2e12f8de9f7
sambiss1 <samyombobiss@gmail.com>
2022-01-21 01:38:08 +0100

    commit

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,1 +1,23 @@
-var container = document.getElementsByClassNames("container");
+/* Toggle menu function */
+function togglemenu() {
+    var x = document.getElementById("menu_content"),
+        navigation = document.getElementById("navigation"),
+        ul = document.getElementById("navigation_items"),
+        header_content = document.getElementById("header_content"),
+        toggle_icon = document.getElementById("toggle_icon");
+
+    if (ul.style.display === "block") {
+        ul.setAttribute("style", "display: none !important");
+    } else {
+
+        ul.setAttribute("style", "display: block !important");
+    }
+
+
+
+    /* Scroll to top function */
+    scrollTotop = document.getElementById("scrollToTop");
+
+    function scrollTop() {
+        window.scrollTo
+    }
